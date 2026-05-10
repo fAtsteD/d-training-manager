9c7799d8f17a28adaa83d4d2d541c9b23aa06e69
Andrii Dats <24232842+fAtsteD@users.noreply.github.com>
2026-05-10 12:57:10 +0000

    Use uuid7 in user data

--- src/d_training_manager/database/user_registry.py
+++ src/d_training_manager/database/user_registry.py
@@ -1,16 +1,13 @@
 import time
-from uuid import uuid4
+from typing import Optional
+from uuid import uuid7
 
 from pynamodb.exceptions import DoesNotExist
 
 from d_training_manager.database.models import DBUser
 
 
-class UserRegistry:
-    pass
-
-
-def get_by_id(user_id: str) -> DBUser | None:
+def get_by_id(user_id: str) -> Optional[DBUser]:
     try:
         return DBUser.get(user_id, consistent_read=True)
     except DoesNotExist:
@@ -19,19 +16,19 @@ def get_by_id(user_id: str) -> DBUser | None:
     return None
 
 
-def get_by_phone(phone: str) -> DBUser | None:
+def get_by_phone(phone: str) -> Optional[DBUser]:
     result = DBUser.phone_index.query(phone, limit=1)
     return next(result, None)
 
 
-def get_by_telegram_id(telegram_id: int) -> DBUser | None:
+def get_by_telegram_id(telegram_id: int) -> Optional[DBUser]:
     result = DBUser.telegram_index.query(telegram_id, limit=1)
     return next(result, None)
 
 
 def update(user: DBUser):
     if not user.id:
-        user.id = str(uuid4())
+        user.id = str(uuid7())
 
     if not user.created_at:
         user.created_at = int(time.time())
